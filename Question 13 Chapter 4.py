ff57124d083b591784622e21a2da2aadc1b62d05
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2019-11-02 14:30:17 -0400

    Added Test Error Rate

--- Question 13 Chapter 4.py
+++ Question 13 Chapter 4.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
@@ -10,16 +11,20 @@ import seaborn as sns
 boston_data = pd.read_csv('Boston.csv', sep=',')
 boston_data.drop('Unnamed: 0', axis=1, inplace=True)
 print(boston_data.head())
+print(boston_data.info())
 
 # Lets perform some analysis first
 sns.pairplot(boston_data.corr())
 plt.show()
 # Coolwarm color map is the easiest to quickly notice correlations
-sns.heatmap(boston_data.corr().round(2), annot=True, cmap='coolwarm')
+sns.heatmap(boston_data.corr().round(1), annot=True, cmap='coolwarm')
+print(boston_data.corr().round(2))
 plt.show()
 
 # Create a binary variable where if the crim value is below the median crim value; it is classified as 0, otherwise 1.
 boston_data['Crim01'] = boston_data['crim'].apply(lambda x: 1 if x > boston_data['crim'].median() else 0)
+
+
 
 
 
