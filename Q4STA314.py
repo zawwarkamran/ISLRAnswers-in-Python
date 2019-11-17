655ecf1f8e146706696a02b0d1b818754656f4cb
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2019-11-17 14:22:06 -0500

    fixed statsmodel GLM

--- Q4STA314.py
+++ Q4STA314.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import statsmodels.api as sm
 np.seterr('ignore')
@@ -9,8 +9,10 @@ de = pd.read_csv('default.csv')
 de.drop(['Unnamed: 0', 'student', 'income'], axis=1, inplace=True)
 de['default'].replace(to_replace=['No', 'Yes'], value=[0, 1], inplace=True)
 
-LogisticReg = sm.GLM(endog=de['default'], exog=de['balance'], family=sm.families.Binomial()).fit()
+LogisticReg = sm.formula.glm('default ~ balance', data=de, family=sm.families.Binomial()).fit()
 print(LogisticReg.summary())
+LogisticReg_2 = sm.GLM(endog=de['default'], exog=sm.add_constant(de['balance']), family=sm.families.Binomial()).fit()
+print(LogisticReg_2.summary())
 
 
 def log_likelihood(par):
@@ -18,9 +20,23 @@ def log_likelihood(par):
     return -np.sum((de['default']*xb)-np.log(1+np.exp(xb)))
 
 
-MLE = minimize(log_likelihood, x0=np.array([0, 0]))
-print(MLE.x)
+MLE = minimize(log_likelihood, x0=np.array([0, 0]), method='Nelder-Mead')
+print(MLE)
 
 
+# Bernoulli
+values = np.array([1, 1, 1, 0, 0])
 
+def  bernloglik(par):
+    return -np.sum(values*np.log(par)+(1-values)*np.log(1-par))
+
+MLE_2 = minimize(bernloglik, x0=np.array([0]), method='Nelder-Mead')
+print(MLE_2)
+
+parvals = np.linspace(0, 1, 20)
+funcvals = list(map(bernloglik, parvals))
+
+plt.plot(parvals, funcvals)
+plt.plot(MLE_2.x)
+plt.show()
 
